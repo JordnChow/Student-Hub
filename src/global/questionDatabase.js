bbf6735819130336b03eaef38cf6c973f289dffd
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2024-09-02 14:32:55 +1000

    Update questionDatabase.js

--- src/global/questionDatabase.js
+++ src/global/questionDatabase.js
@@ -28,95 +28,95 @@ export const questionDB = {
       type: "multipleChoice",
       image: "https://a57.foxnews.com/a57.foxnews.com/static.foxnews.com/foxnews.com/content/uploads/2019/03/640/320/1862/1048/iStock-vaccine.jpg?ve=1&tl=1?ve=1&tl=1"
     },
-    // {
-    //   question: "The rats transmitted the disease.",
-    //   answers: {
-    //     a: "True",
-    //     b: "False"
-    //   },
-    //   correctAnswer: "b",
-    //   type: "multipleChoice",
-    //   image: "https://th.bing.com/th/id/R.375dbe6d3f1d14615527b53ce179a8de?rik=M3MtRHtXdHPkBA&riu=http%3a%2f%2flepassetempsderose.l.e.pic.centerblog.net%2f71f4a646.gif&ehk=b1PqphSnGxQuskjvdiWpk2frjqRB5LWJLcOhMIpqxmM%3d&risl=&pid=ImgRaw&r=0"
-    // },
-    // {
-    //   question: "During which time period was The Black Death?",
-    //   answers: {
-    //     a: "18th century",
-    //     b: "10th century",
-    //     c: "14th century",
-    //     d: "13th century"
-    //   },
-    //   correctAnswer: "c",
-    //   type: "multipleChoice",
-    //   image: "https://media.tenor.com/images/0177b951299f12e25b9620c8d8c9bead/tenor.gif"
-    // },
-    // {
-    //   question: "Which best describes the symptoms caused by The Black Death?",
-    //   answers: {
-    //     a: "Dark spots on the skin and swellings",
-    //     b: "Excessive coughing and sneezing",
-    //     c: "Sore throat and blood vomit",
-    //     d: "Fever"
-    //   },
-    //   correctAnswer: ["a", "d"],
-    //   type: "checkmark",
-    //   image: "https://www.verywellhealth.com/thmb/kP5SwDnwHMYInmYKeNe1ZRLFz-g=/6251x4168/filters:no_upscale():max_bytes(150000):strip_icc()/3232847_color1-5c0191cec9e77c00013b3053.png"
-    // },
-    // {
-    //   question: "The Black Death was primarily spread navally.",
-    //   answers: {
-    //     a: "True",
-    //     b: "False"
-    //   },
-    //   correctAnswer: "a",
-    //   type: "multipleChoice",
-    //   image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
-    // },
-    // {
-    //   question: "How did The Black Death get its name?",
-    //   answers: {
-    //     a: "It gives you blackish spots on your skin",
-    //     b: "It makes you black",
-    //     c: "The symptoms are more prevalent at night time.",
-    //     d: "It makes you blind"
-    //   },
-    //   correctAnswer: "a",
-    //   type: "multipleChoice",
-    //   image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/E._coli_Bacteria_(16578744517).jpg"
-    // },
-    // {
-    //   question: "The Black Death was primarily caused by a bacterium called Yersinia pestis.",
-    //   answers: {
-    //     a: "True",
-    //     b: "False"
-    //   },
-    //   correctAnswer: "a",
-    //   type: "multipleChoice",
-    //   image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
-    // },
-    // {
-    //   question: "What were some of the religious responses to the Black Death?",
-    //   answers: {
-    //     a: "The Great Removal of Autism",
-    //     b: "Persecution of minorities",
-    //     c: "Burning of Infected Victims",
-    //     d: "Mass Execution of Fleas"
-    //   },
-    //   correctAnswer: "b",
-    //   type: "multipleChoice",
-    //   image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
-    // },
-    // {
-    //   question: "Which of the following countries were affected by the plague? Select all the correct options.",
-    //   answers: {
-    //     a: "England",
-    //     b: "Poland",
-    //     c: "Nigeria",
-    //     d: "Australia"
-    //   },
-    //   correctAnswer: ["a", "b"],
-    //   type: "checkmark"
-    // }
+    {
+      question: "The rats transmitted the disease.",
+      answers: {
+        a: "True",
+        b: "False"
+      },
+      correctAnswer: "b",
+      type: "multipleChoice",
+      image: "https://th.bing.com/th/id/R.375dbe6d3f1d14615527b53ce179a8de?rik=M3MtRHtXdHPkBA&riu=http%3a%2f%2flepassetempsderose.l.e.pic.centerblog.net%2f71f4a646.gif&ehk=b1PqphSnGxQuskjvdiWpk2frjqRB5LWJLcOhMIpqxmM%3d&risl=&pid=ImgRaw&r=0"
+    },
+    {
+      question: "During which time period was The Black Death?",
+      answers: {
+        a: "18th century",
+        b: "10th century",
+        c: "14th century",
+        d: "13th century"
+      },
+      correctAnswer: "c",
+      type: "multipleChoice",
+      image: "https://media.tenor.com/images/0177b951299f12e25b9620c8d8c9bead/tenor.gif"
+    },
+    {
+      question: "Which best describes the symptoms caused by The Black Death?",
+      answers: {
+        a: "Dark spots on the skin and swellings",
+        b: "Excessive coughing and sneezing",
+        c: "Sore throat and blood vomit",
+        d: "Fever"
+      },
+      correctAnswer: ["a", "d"],
+      type: "checkmark",
+      image: "https://www.verywellhealth.com/thmb/kP5SwDnwHMYInmYKeNe1ZRLFz-g=/6251x4168/filters:no_upscale():max_bytes(150000):strip_icc()/3232847_color1-5c0191cec9e77c00013b3053.png"
+    },
+    {
+      question: "The Black Death was primarily spread navally.",
+      answers: {
+        a: "True",
+        b: "False"
+      },
+      correctAnswer: "a",
+      type: "multipleChoice",
+      image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
+    },
+    {
+      question: "How did The Black Death get its name?",
+      answers: {
+        a: "It gives you blackish spots on your skin",
+        b: "It makes you black",
+        c: "The symptoms are more prevalent at night time.",
+        d: "It makes you blind"
+      },
+      correctAnswer: "a",
+      type: "multipleChoice",
+      image: "https://upload.wikimedia.org/wikipedia/commons/d/d5/E._coli_Bacteria_(16578744517).jpg"
+    },
+    {
+      question: "The Black Death was primarily caused by a bacterium called Yersinia pestis.",
+      answers: {
+        a: "True",
+        b: "False"
+      },
+      correctAnswer: "a",
+      type: "multipleChoice",
+      image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
+    },
+    {
+      question: "What were some of the religious responses to the Black Death?",
+      answers: {
+        a: "The Great Removal of Autism",
+        b: "Persecution of minorities",
+        c: "Burning of Infected Victims",
+        d: "Mass Execution of Fleas"
+      },
+      correctAnswer: "b",
+      type: "multipleChoice",
+      image: "https://i2-prod.walesonline.co.uk/whats-on/whats-on-news/article11349422.ece/ALTERNATES/s1200/matthew-ship.jpg"
+    },
+    {
+      question: "Which of the following countries were affected by the plague? Select all the correct options.",
+      answers: {
+        a: "England",
+        b: "Poland",
+        c: "Nigeria",
+        d: "Australia"
+      },
+      correctAnswer: ["a", "b"],
+      type: "checkmark"
+    }
   ]
 };
 
