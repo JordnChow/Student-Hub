355a33bd530dfc21c6a553e6358e2014a152e0d6
MaxGa1ea <max.galea@sydstu.catholic.edu.au>
2024-10-30 11:18:51 +1100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -107,7 +107,16 @@ function LoadSignUp() {
 function SignUp() {
 	const Alert = document.getElementById("alert")
 	if (document.getElementById("Email").value && document.getElementById("Password").value) {
-		document.getElementById("my_captcha_form").submit()
+		// document.getElementById("my_captcha_form").submit()
+		var response = grecaptcha.getResponse();
+		if(response.length == 0) 
+		{ 
+		//reCaptcha not verified
+		evt.preventDefault();
+		return false;
+		}
+		//reCaptch Verified
+		sessionStorage.setItem("ReCaptcha", "Yes")
 		if (sessionStorage.getItem("ReCaptcha") == "Yes") {
 			const Email = document.getElementById("Email").value
 			const Password = document.getElementById("Password").value
